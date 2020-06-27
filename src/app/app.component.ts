5f62140ac2b91ba76153f6e7963c538319347856
Pankaj <pankajagrawal.kkb@gmail.com>
2020-06-27 12:00:04 +0530

    data push of advantal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+var a, b, c, d = '' ;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'advantaltest';
+  perSelect:any = "";
+  data:any;
+
+  onSelect(v){
+    if(!a){ a = ""; }
+    if(!b){ b = ""; }
+    if(!c){ c = ""; }
+    if(!d){ d = ""; }
+   
+    if(a.length < 7){
+        a += v;
+    }else if (a.length <= 7 && b.length < 6){
+        if(b.length == 2){
+          b+=" ";
+          return;
+        }
+        b+=v;
+    }
+    else if(b.length >= 5 && c.length == 0){
+        c=v;
+    }else if(c.length == 1 && d.length == 0){
+        d=v;
+    }
+      this.data = [{a},{b},{c},{d}];
+      // console.log(this.ss)
+  }
+
 }
